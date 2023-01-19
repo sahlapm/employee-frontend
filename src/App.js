882d2c68cc920d7095f216eca96bc070a04dba6d
Sahla <sahlapm@gmail.om>
2023-01-19 09:07:38 +0300

    employee frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import EmployeeForm from './components/EmployeeForm';
+import Home from './components/Home';
+import { Link,Routes,Route ,BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Router>
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+            <div class="collapse navbar-collapse Mainheader Mainheading" id="navbarTogglerDemo01">
+             
+              <h3 class="text-white  heading">Employee App </h3>
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+              </ul>
+    
+             
+    
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item active nav-item">
+                  <Link className='nav-link text-white navlnk' to='/'>Home</Link>
+                </li>
+    
+                <li class="nav-item active nav-item">
+                  <Link className='nav-link text-white navlnk' to='/employee'>Employee</Link>
+                </li>
+    
+               
+    
+              
+    
+              </ul>
+            </div>
+          </nav>
+          
+    
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/employee" element={<EmployeeForm/>} />
+     
+     </Routes>
+     
+ </Router>
+     
+
+     </div>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import {Table} from 'semantic-ui-react';
+
+const Home = () => {
+      const [user, setUser] = useState([]);
+
+  const fetchData = () => { 
+    return fetch("https://jsonplaceholder.typicode.com/users")
+          .then((response) => response.json())
+          .then((data) => setUser(data));
+  }
+  useEffect(() => {
+    fetchData();
+  },[])
+
+  return (
+    <div>
+
+ <section class="Background">
+      
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col ">
+            <div class="card card-table" >
+              <div class="row g-0">
+              <div class="d-flex justify-content-center pt-3">
+              <h1 class="fw-Bolder mb-3 pb-3 headeing" >Employee List</h1>
+              </div>
+              <Table celled padded>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Id</Table.HeaderCell>
+        <Table.HeaderCell>Name</Table.HeaderCell>
+        <Table.HeaderCell>Email</Table.HeaderCell>
+        
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+
+ {user.map(userObj=>{
+return(
+  <Table.Row key={userObj.id.toString()}>
+        <Table.Cell>{userObj.id}</Table.Cell>
+        <Table.Cell>{userObj.name}</Table.Cell>
+        <Table.Cell>{userObj.email}</Table.Cell>
+        
+      </Table.Row>
+)
+
+     })}
+    
+    </Table.Body>
+   
+  </Table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </section>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
